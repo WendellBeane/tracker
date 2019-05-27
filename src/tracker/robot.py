9aeb1a0d3bf3d60d9507cc022e5f4e749cf6d4b9
jennifer jain <jennifer.jain.th@dartmouth.edu>
2019-05-27 02:12:29 -0400

    added camera and attempted to get blobfinder to work, (now commented)

--- src/tracker/robot.py
+++ src/tracker/robot.py
@@ -14,6 +14,14 @@ from sensor_msgs.msg import LaserScan
 class Robot:
     def __init__(self, freq=1):
 
+        # initialize subscriber for static target (this code assumes that target is known)
+        rospy.Subscriber("/robot_0/base_pose_ground_truth", Odometry, self.odometry_callback_target)
+
+        # target pose variables
+        self.x_target = None
+        self.y_target = None
+        # self.yaw_target = None    # TODO: might want to account for this later when determining goal points for each robot
+
         # initialize subscribers (for hunters)
         rospy.Subscriber("/robot_1/base_pose_ground_truth", Odometry, self.odometry_callback_1)
         rospy.Subscriber("/robot_1/base_scan", LaserScan, self.laser_scan_callback_1)
@@ -23,15 +31,35 @@ class Robot:
         self.y = None
         self.yaw = None
 
-        print("in the init method")
+        # behavioral state machine
+        self.curr_state = "COMPUTE_PTS"
+        self.next_state = None
 
 
     def spin(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
             rospy.loginfo("X: " + str(self.x) + " Y: " + str(self.y) + " YAW: " + str(self.yaw))
+            rospy.loginfo("target_X: " + str(self.x_target) + " target_Y: " + str(self.y_target))
+
+
             r.sleep()
 
+
+    def odometry_callback_target(self, odometry_msg):
+        pose = odometry_msg.pose.pose
+        quaternion = (
+            pose.orientation.x,
+            pose.orientation.y,
+            pose.orientation.z,
+            pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        yaw = euler[2]
+
+        # update invidual robot's position
+        self.x_target = pose.position.x
+        self.y_target = pose.position.y
+        self.yaw_target = yaw
 
 
     def odometry_callback_1(self, odometry_msg):
